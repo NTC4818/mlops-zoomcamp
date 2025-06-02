996a6551c2124f568cda93184c82d456aad2d92f
NTC4818 <75403286+NTC4818@users.noreply.github.com>
2025-06-02 01:43:00 +0000

    question 4

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,10 +1,13 @@
 import os
 import pickle
 import click
+import mlflow
+import logging
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 
+logging.basicConfig(level=logging.DEBUG)
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -19,14 +22,22 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    # Enable autologging for scikit-learn
+    mlflow.sklearn.autolog()
+    
+    #start mlflow run
+    with mlflow.start_run():
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rmse = root_mean_squared_error(y_val, y_pred)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
+        # You can also manually log additional metrics or parameters if needed
+        mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
